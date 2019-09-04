8895ddc54f9d6f55838d224de8caf2ac3fec0e8e
David Platten <dplatten@gmail.com>
2019-09-04 10:54:29 +0100

    Added highlighting of the entire table row when a fluoroscopy alert level is exceeded. References issue #771

---
+++ openrem/remapp/templatetags/update_variable.py
@@ -0,0 +1,18 @@
+# This Python file uses the following encoding: utf-8
+
+"""
+..  module:: update_variable
+    :synopsis: template assignment tag to change the value of a template variable
+"""
+
+from django import template
+register = template.Library()
+
+@register.assignment_tag()
+def update_variable(value):
+    """ Template assignment tag to return a supplied value to enable template variable values to be updated
+
+    :param value: the value to return
+    :return: the received value parameter
+    """
+    return value
