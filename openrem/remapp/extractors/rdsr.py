216c7319af11d901e9d5cbdf5ab991db0ac54abb
Ed McDonagh <ed@mcdonagh.org.uk>
2019-09-29 21:10:40 +0100

    Fixed total DLP summary field population for new imports. Refs #778

--- openrem/remapp/extractors/rdsr.py
+++ openrem/remapp/extractors/rdsr.py
@@ -1371,6 +1371,7 @@ def _generalstudymoduleattributes(dataset, g, ch):
         ct_event_type_count(g)
         try:
             g.total_dlp = g.ctradiationdose_set.get().ctaccumulateddosedata_set.get().ct_dose_length_product_total
+            g.save()
         except ObjectDoesNotExist:
             logger.warning(u"Study UID {0} of modality {1}. Unable to set summary total_dlp".format(
                 g.study_instance_uid, get_value_kw("ManufacturerModelName", dataset)))
