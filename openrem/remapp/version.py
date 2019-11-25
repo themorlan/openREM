be4ff09ca6866185fc7cb6f66dc1346827b4c18b
Ed McDonagh <ed@mcdonagh.org.uk>
2019-11-25 22:42:25 +0000

    Added UIDs to version.py. Refs #530. [skip ci] netdicom testing disabled.

--- openrem/remapp/version.py
+++ openrem/remapp/version.py
@@ -6,3 +6,7 @@ __netdicom_implementation_version__ = "0.10.0.0"  # Used as part of UID when sto
 __repo_branch__ = "0.10.0"            # __repo_branch__feeds into construction of link in netdicom-orthanc-config doc
                                         # to orthanc example configuration on bitbucket. Should be set to tag name
                                         # at release, not branch name.
+# IANA OID for OpenREM, + 1 + major version + minor + patch + iteration (increment for betas if pydicom code changes)
+__implementation_uid__ = "1.3.6.1.4.1.45593.1.0.10.0.1"
+# IANA private enterprise number for OpenREM, + 2
+__openrem_root_uid__ = "1.3.6.1.4.1.45593.2."
