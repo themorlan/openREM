17169c9603bbdb7c38fc677a4fe6571f3708714d
Ed McDonagh <ed@mcdonagh.org.uk>
2020-08-12 22:32:44 +0100

    Created BROKER_URL variable. Refs #849

--- openrem/openremproject/settings.py
+++ openrem/openremproject/settings.py
@@ -44,7 +44,7 @@ JS_REVERSE_OUTPUT_PATH = os.path.join(STATIC_ROOT, "js", "django_reverse")
 VIRTUAL_DIRECTORY = os.environ.get("VIRTUAL_DIRECTORY", default="")
 
 # Celery settings
-BROKER_URL = "amqp://guest:guest@broker:5672//"
+BROKER_URL = os.environ.get("BROKER_URL", default="amqp://guest:guest@localhost:5672//")
 BROKER_MGMT_URL = os.environ.get("BROKER_MGMT_URL", default="http://localhost:15672/")
 
 CELERY_ACCEPT_CONTENT = ["json"]
