f9207119d2f3296a2762cb405c10c9b85f1b083c
Ed McDonagh <ed@mcdonagh.org.uk>
2020-09-04 12:18:58 +0100

    Adding initial German translation of docs. Refs #512 [skip ci] docs only

--- docs/conf.py
+++ docs/conf.py
@@ -280,6 +280,9 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
 
+gettext_uuid = True
+gettext_compact = False
+
 
 autodoc_member_order = "bysource"
 
