c447d1d33ceeeb555e09f92ac33595dd344db8a9
David Platten <dplatten@gmail.com>
2020-09-04 08:54:34 +0100

    Removed unnecessary JavaScript global variables. References issue #477

--- openrem/remapp/static/js/charts/ctChartAjax.js
+++ openrem/remapp/static/js/charts/ctChartAjax.js
@@ -8,7 +8,6 @@ urlStartReq, urlStartStudy, updateOverTimeChart, chartSortingDirection*/
 // Code to update the page and chart data on initial page load.
 $(document).ready(function() {
     var requestData = arrayToURL(urlToArray(this.URL));
-    var i;
 
     $(".ajax-progress").show();
 
@@ -18,31 +17,29 @@ $(document).ready(function() {
         data: requestData,
         dataType: "json",
         success: function( json ) {
-            // Initialise some colours to use for plotting
-            var colourScale = chroma.scale("RdYlBu");
 
             // DLP per acquisition chart data
-            if(typeof plotCTAcquisitionMeanDLP !== "undefined") {
+            if(typeof json.acquisitionDLPData !== "undefined") {
                 vegaEmbed('#acquisitionAverageDLPChartDiv',  JSON.parse(json.acquisitionDLPData)).catch(console.error);
             }
 
             // CTDI per acquisition chart data
-            if(typeof plotCTAcquisitionMeanCTDI !== "undefined") {
+            if(typeof json.acquisitionCTDIData !== "undefined") {
                 vegaEmbed('#acquisitionAverageCTDIChartDiv',  JSON.parse(json.acquisitionCTDIData)).catch(console.error);
             }
 
             // DLP per study chart data
-            if(typeof plotCTStudyMeanDLP !== "undefined") {
+            if(typeof json.studyDLPData !== "undefined") {
                 vegaEmbed('#studyAverageDLPChartDiv',  JSON.parse(json.studyDLPData)).catch(console.error);
             }
 
             // CTDI per study chart data
-            if(typeof plotCTStudyMeanCTDI !== "undefined") {
+            if(typeof json.studyCTDIData !== "undefined") {
                 vegaEmbed('#studyAverageCTDIChartDiv',  JSON.parse(json.studyCTDIData)).catch(console.error);
             }
 
             // DLP per request chart data start
-            if(typeof plotCTRequestMeanDLP !== "undefined") {
+            if(typeof json.requestData !== "undefined") {
                 vegaEmbed('#requestAverageDLPChartDiv',  JSON.parse(json.requestData)).catch(console.error);
                 if(typeof json.requestHistData !== "undefined") {
                     vegaEmbed('#requestHistogramDLPChartDiv', JSON.parse(json.requestHistData)).catch(console.error);
@@ -50,38 +47,38 @@ $(document).ready(function() {
             }
 
             // Number of events per study chart data
-            if(typeof plotCTStudyNumEvents !== "undefined") {
+            if(typeof json.studyNumEventsData !== "undefined") {
                 vegaEmbed('#studyAverageNumEventsChartDiv',  JSON.parse(json.studyNumEventsData)).catch(console.error);
             }
 
             // Number of events per request chart data
-            if(typeof plotCTRequestNumEvents !== "undefined") {
+            if(typeof json.requestNumEventsData !== "undefined") {
                 vegaEmbed('#requestAverageNumEventsChartDiv',  JSON.parse(json.requestNumEventsData)).catch(console.error);
             }
 
             // Acquisition frequency chart data start
-            if(typeof plotCTAcquisitionFreq !== "undefined" || typeof plotCTAcquisitionMeanCTDI !== "undefined") {
+            if(typeof json.acquisitionFreqData !== "undefined") {
                 vegaEmbed('#acquisitionFreqChartDiv',  JSON.parse(json.acquisitionFreqData)).catch(console.error);
             }
 
             // Study frequency chart data start
-            if(typeof plotCTStudyFreq !== "undefined") {
+            if(typeof json.studyFreqData !== "undefined") {
                 vegaEmbed('#studyFreqChartDiv',  JSON.parse(json.studyFreqData)).catch(console.error);
             }
 
             // Request frequency chart data start
-            if(typeof plotCTRequestFreq !== "undefined") {
+            if(typeof json.requestFreqData !== "undefined") {
                 vegaEmbed('#requestFreqChartDiv',  JSON.parse(json.requestFreqData)).catch(console.error);
             }
 
             // DLP over time chart data
-            if(typeof plotCTStudyMeanDLPOverTime !== "undefined") {
+            if(typeof json.studyDLPoverTime !== "undefined") {
                 vegaEmbed('#studyAverageDLPOverTimeChartDiv',  JSON.parse(json.studyDLPoverTime)).catch(console.error);
             }
 
             // Study workload chart data
-            if(typeof plotCTStudyPerDayAndHour !== "undefined") {
-                updateWorkloadChart(json.studiesPerHourInWeekdays, "piechartStudyWorkloadDIV", colourScale);
+            if(typeof json.studiesPerHourInWeekdays !== "undefined") {
+                vegaEmbed('#piechartStudyWorkloadDIV',  JSON.parse(json.studiesPerHourInWeekdays)).catch(console.error);
             }
 
             $(".ajax-progress").hide();
