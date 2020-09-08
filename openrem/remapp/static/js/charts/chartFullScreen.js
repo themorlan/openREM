8066a1e0971323d614dd0c813d38e195eb180c0a
David Platten <dplatten@gmail.com>
2020-09-08 16:20:00 +0100

    Trying to make a full screen toggle button for the Plotly chart. Not quite there yet, but nearly. References issue #477

--- openrem/remapp/static/js/charts/chartFullScreen.js
+++ openrem/remapp/static/js/charts/chartFullScreen.js
@@ -1,4 +1,5 @@
 var chartStartHeight = 0;
+var chartStartWidth = 0;
 var chartStartFooterHeight = 0;
 var chartFullScreen = false;
 
@@ -36,7 +37,7 @@ function fitPlotlyChartToDiv(chartGrandParentDiv) {
 
     if (chartGrandParentDivElement.width() && chartGrandParentDivElement.height()) {
 
-        var num_divs = chartGrandParentDivElement.children().length;
+        var num_divs = chartGrandParentDivElement.children().length - 1;
 
         for (i = 0; i < num_divs; i++) {
             var chart_div_id = $((chartGrandParentDivElement.children()[i]).innerHTML).attr("id")
@@ -47,4 +48,32 @@ function fitPlotlyChartToDiv(chartGrandParentDiv) {
             Plotly.relayout(chart_div_id, update);
         }
     }
+}
+
+
+function enterFullScreenPlotly(chartGrandParentDivId) {
+    var chartGrandParentDivElement = $("#" + chartGrandParentDivId);
+
+    var chart_div_id = $((chartGrandParentDivElement.children()[0]).innerHTML).attr("id")
+
+    if (chartFullScreen === false) {
+        chartStartHeight = document.getElementById(chart_div_id).clientHeight;
+        chartStartWidth = document.getElementById(chart_div_id).clientWidth;
+
+        chartStartFooterHeight = document.getElementById(chartGrandParentDivId).clientHeight - chartStartHeight;
+
+        document.getElementById(chartGrandParentDivId).style.height = $(window).height() + "px";
+        document.getElementById(chartGrandParentDivId).style.width = $(window).width() + "px";
+
+        chartFullScreen = true;
+    }
+    else {
+        document.getElementById(chartGrandParentDivId).style.height = chartStartHeight + chartStartFooterHeight + "px";
+        document.getElementById(chartGrandParentDivId).style.width = chartStartWidth + "px";
+
+        chartFullScreen = false;
+    }
+
+    $("#"+chartGrandParentDivId).toggleClass("fullscreen");
+    fitPlotlyChartToDiv(chartGrandParentDivId);
 }
