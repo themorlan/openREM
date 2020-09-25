bd747f3cfce5881a5921ce6b310ded0958e1e6ed
Ed McDonagh <ed@mcdonagh.org.uk>
2020-09-25 22:23:53 +0100

    Updating Celery settings to lower case; previous will be deprecated in Celery 6.0. Refs #857 because need to upgrade Celery to avoid Sphinx warnings.

--- openrem/openremproject/settings.py
+++ openrem/openremproject/settings.py
@@ -39,19 +39,19 @@ JS_REVERSE_OUTPUT_PATH = os.path.join(STATIC_ROOT, "js", "django_reverse")
 VIRTUAL_DIRECTORY = os.environ.get("VIRTUAL_DIRECTORY", default="")
 
 # Celery settings
-BROKER_URL = os.environ.get("BROKER_URL", default="amqp://guest:guest@localhost:5672//")
+broker_url = os.environ.get("BROKER_URL", default="amqp://guest:guest@localhost:5672//")
 BROKER_MGMT_URL = os.environ.get("BROKER_MGMT_URL", default="http://localhost:15672/")
 
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_DEFAULT_QUEUE = "default"
-CELERYD_PREFETCH_MULTIPLIER = 1
+accept_content = ["json"]
+task_serializer = "json"
+result_serializer = "json"
+task_default_queue = "default"
+worker_prefetch_multiplier = 1
 
 FLOWER_PORT = int(os.environ.get("FLOWER_PORT", default=5555))
 FLOWER_URL = os.environ.get("FLOWER_URL", default="http://localhost")
 
-CELERYBEAT_SCHEDULE = {
+beat_schedule = {
     "trigger-dicom-keep-alive": {
         "task": "remapp.netdicom.keepalive.keep_alive",
         "schedule": crontab(minute="*/1"),
