cc498b87f71481ed7bef30df7e208721af5e9e76
Ed McDonagh <ed@mcdonagh.org.uk>
2020-09-26 10:49:39 +0100

    Import errors in building docs. Refs #857 [skip ci] docs only

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'openrem')))
 from openrem.remapp.version import __version__, __short_version__, __repo_branch__
 
 # -- Project information -----------------------------------------------------
