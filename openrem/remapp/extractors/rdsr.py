defa3d00a55ffcfa0b8e19097d76d1c7a35fb971
Ed McDonagh <ed@mcdonagh.org.uk>
2020-11-24 23:01:09 +0000

    Missed =, still not sure if both will be respected. Refs #865

--- openrem/remapp/extractors/rdsr.py
+++ openrem/remapp/extractors/rdsr.py
@@ -72,7 +72,7 @@ if projectpath not in sys.path:
 os.environ["DJANGO_SETTINGS_MODULE"] = "openremproject.settings"
 django.setup()
 
-from remapp.models import (  # pylint: disable wrong-import-order, wrong-import-position
+from remapp.models import (  # pylint: disable=wrong-import-order, wrong-import-position
     AccumCassetteBsdProjRadiogDose,
     AccumIntegratedProjRadiogDose,
     AccumMammographyXRayDose,
