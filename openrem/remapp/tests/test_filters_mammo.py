f2c2d6f8e3417724fb393c867b51c9cbfe9e1773
David Platten <dplatten@gmail.com>
2021-01-05 15:24:58 +0000

    Added tests for the new mammography filters. Refs issue #861

--- openrem/remapp/tests/test_filters_mammo.py
+++ openrem/remapp/tests/test_filters_mammo.py
@@ -24,14 +24,17 @@ class FilterViewTests(TestCase):
         mam1 = "test_files/MG-Im-Hologic-PropProj.dcm"
         mam2 = "test_files/MG-Im-GE-SenDS-scaled.dcm"
         mam3 = "test_files/MG-RDSR-Hologic_2D.dcm"
+        mam4 = "test_files/MG-RDSR-Hologic_mix.dcm"
         root_tests = os.path.dirname(os.path.abspath(__file__))
         path_mam1 = os.path.join(root_tests, mam1)
         path_mam2 = os.path.join(root_tests, mam2)
         path_mam3 = os.path.join(root_tests, mam3)
+        path_mam4 = os.path.join(root_tests, mam4)
 
         mam.mam(path_mam1)
         mam.mam(path_mam2)
         rdsr.rdsr(path_mam3)
+        rdsr.rdsr(path_mam4)
 
     def test_list_all_mammo(self):
         """
@@ -40,7 +43,7 @@ class FilterViewTests(TestCase):
         self.client.login(username="temporary", password="temporary")
         response = self.client.get(reverse("mg_summary_list_filter"), follow=True)
         self.assertEqual(response.status_code, 200)
-        three_responses_text = u"There are 3 studies in this list."
+        three_responses_text = u"There are 4 studies in this list."
         self.assertContains(response, three_responses_text)
 
     def test_filter_study_desc(self):
@@ -93,5 +96,98 @@ class FilterViewTests(TestCase):
         self.assertContains(response, one_responses_text)
         accession_number = (
             u"AAAA9876"  # Accession number of study with matching acquisition protocol
+        )
+        self.assertContains(response, accession_number)
+
+    def test_filter_exposure_control_mode(self):
+        """
+        Apply exposure control mode filter
+        """
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get(
+            reverse("mg_summary_list_filter")
+            + "?projectionxrayradiationdose__irradeventxraydata__irradeventxraysourcedata__exposure_control_mode=automatic",
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        one_responses_text = u"There are 1 studies in this list."
+        self.assertContains(response, one_responses_text)
+        accession_number = (
+            u"AAAA9876"  # Accession number of the only study with a matching exposure control mode
+        )
+        self.assertContains(response, accession_number)
+
+    def test_filter_compressed_breast_thickness_a(self):
+        """
+        Apply compressed breast thickness filter
+        """
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get(
+            reverse("mg_summary_list_filter")
+            + "?projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_min=0"
+            + "&projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_max=18",
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        one_responses_text = u"There are 1 studies in this list."
+        self.assertContains(response, one_responses_text)
+        accession_number = (
+            u"90002314523"  # Accession number of the only study with a compressed breast thickness in the range
+        )
+        self.assertContains(response, accession_number)
+
+    def test_filter_compressed_breast_thickness_b(self):
+        """
+        Apply compressed breast thickness filter
+        """
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get(
+            reverse("mg_summary_list_filter")
+            + "?projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_min=42"
+            + "&projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_max=44",
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        one_responses_text = u"There are 1 studies in this list."
+        self.assertContains(response, one_responses_text)
+        accession_number = (
+            u"AJSKDL1234"  # Accession number of the only study with a compressed breast thickness in the range
+        )
+        self.assertContains(response, accession_number)
+
+    def test_filter_compressed_breast_thickness_c(self):
+        """
+        Apply compressed breast thickness filter
+        """
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get(
+            reverse("mg_summary_list_filter")
+            + "?projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_min=53"
+            + "&projectionxrayradiationdose__irradeventxraydata__irradeventxraymechanicaldata__compression_thickness__range_max=100",
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        one_responses_text = u"There are 1 studies in this list."
+        self.assertContains(response, one_responses_text)
+        accession_number = (
+            u"AAAA9876"  # Accession number of the only study with a compressed breast thickness in the range
+        )
+        self.assertContains(response, accession_number)
+
+    def test_filter_view_code_meaning(self):
+        """
+        Apply exposure control mode filter
+        """
+        self.client.login(username="temporary", password="temporary")
+        response = self.client.get(
+            reverse("mg_summary_list_filter")
+            + "?projectionxrayradiationdose__irradeventxraydata__image_view__code_meaning=lateral",
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        one_responses_text = u"There are 1 studies in this list."
+        self.assertContains(response, one_responses_text)
+        accession_number = (
+            u"6414000490125177"  # Accession number of the only study with an acquisition that has a matching view code
         )
         self.assertContains(response, accession_number)
