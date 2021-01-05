0d2cfe198ae635b852a8b09ce176d7a73bc972aa
David Platten <dplatten@gmail.com>
2021-01-05 16:45:31 +0000

    Trying to disable too-many-lines for the whole of the mod_filters file [skip ci]. Refs issue #861

--- openrem/remapp/interface/mod_filters.py
+++ openrem/remapp/interface/mod_filters.py
@@ -1,4 +1,4 @@
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long, disable=too-many-lines
 #    OpenREM - Radiation Exposure Monitoring tools for the physicist
 #    Copyright (C) 2012,2013  The Royal Marsden NHS Foundation Trust
 #
