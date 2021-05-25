ac28047cd1ab6036350734d4d8f7a3cbb68d3e95
David Platten <dplatten@gmail.com>
2021-05-25 15:40:40 +0100

    Added patient weight filters for the radiographic summary page. Refs issue #903

--- openrem/remapp/interface/mod_filters.py
+++ openrem/remapp/interface/mod_filters.py
@@ -880,6 +880,16 @@ class DXSummaryListFilter(django_filters.FilterSet):
         label="Max age (yrs)",
         field_name="patientstudymoduleattr__patient_age_decimal",
     )
+    patientstudymoduleattr__patient_weight__gte = django_filters.NumberFilter(
+        lookup_expr="gte",
+        label="Min weight (kg)",
+        field_name="patientstudymoduleattr__patient_weight",
+    )
+    patientstudymoduleattr__patient_weight__lte = django_filters.NumberFilter(
+        lookup_expr="lte",
+        label="Max weight (kg)",
+        field_name="patientstudymoduleattr__patient_weight",
+    )
     generalequipmentmoduleattr__institution_name = django_filters.CharFilter(
         lookup_expr="icontains", label="Hospital"
     )
@@ -943,6 +953,8 @@ class DXSummaryListFilter(django_filters.FilterSet):
             "generalequipmentmoduleattr__station_name",
             "patientstudymoduleattr__patient_age_decimal__gte",
             "patientstudymoduleattr__patient_age_decimal__lte",
+            "patientstudymoduleattr__patient_weight__gte",
+            "patientstudymoduleattr__patient_weight__lte",
             "accession_number",
             "study_dap_min",
             "study_dap_max",
