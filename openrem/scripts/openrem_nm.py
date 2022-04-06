0a8bcce9b7d0dd6728a8d52e5cb8856d3fce790c
Jannis Widmer <jannis.widmer@ksa.ch>
2022-04-06 13:57:33 +0200

    Added openrem_nm to scripts as a means to trigger the import via console. Added the new script to setup.py

---
+++ openrem/scripts/openrem_nm.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+# This Python file uses the following encoding: utf-8
+# scripts/openrem_rdsr
+
+"""Script to launch the rdsr to import information from DICOM Radiation SR objects 
+
+    :param filename: relative or absolute path to Radiation Dose Structured Report.
+    :type filename: str.
+
+    Tested with:
+        * CT: Siemens, Philips and GE RDSR, GE Enhanced SR.
+        * Fluoro: Siemens Artis Zee RDSR
+"""
+
+import sys
+from glob import glob
+from openrem.remapp.extractors.nm_image import nm_image
+
+if len(sys.argv) < 2:
+    sys.exit(
+        "Error: Supply at least one argument - the nuclear medicine image"
+    )
+
+for arg in sys.argv[1:]:
+    for filename in glob(arg):
+        nm_image(arg)
+
+sys.exit()
