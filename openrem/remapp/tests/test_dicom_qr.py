11cd2afed1662db193f72aaa94ba357c8e3bdf5c
Jannis Widmer <jannis.widmer@ksa.ch>
2022-04-21 08:52:14 +0200

    Changed tests to work with the new behavior of including all series for PET/NM. Added a special case to qrscu allowing to retry query for the first series if instance number does not start with 1 but with 0.

--- openrem/remapp/tests/test_dicom_qr.py
+++ openrem/remapp/tests/test_dicom_qr.py
@@ -1371,9 +1371,11 @@ class PruneSeriesResponseNM(TestCase):
         studies = query.dicomqrrspstudy_set.all()
         self.assertEqual(studies.count(), 1)
         series = studies[0].dicomqrrspseries_set.all()
-        self.assertEqual(series.count(), 1)
-        self.assertEqual(series.get().modality, "SR")
-
+        self.assertEqual(series.count(), 3)
+        for serie in series.all():
+            self.assertIn(serie.modality, ["SR", "PT", "NM"])
+        
+    
     @patch("remapp.netdicom.qrscu._query_images", _fake_image_query)
     def test_prune_ser_resp_pt_image(self):
         """
@@ -1400,8 +1402,9 @@ class PruneSeriesResponseNM(TestCase):
         studies = query.dicomqrrspstudy_set.all()
         self.assertEqual(studies.count(), 1)
         series = studies[0].dicomqrrspseries_set.all()
-        self.assertEqual(series.count(), 1)
-        self.assertEqual(series.get().modality, "PT")
+        self.assertEqual(series.count(), 2)
+        for serie in series.all():
+            self.assertIn(serie.modality, ["PT", "NM"])
 
     @patch("remapp.netdicom.qrscu._query_images", _fake_image_query)
     def test_prune_ser_resp_nm_image(self):
