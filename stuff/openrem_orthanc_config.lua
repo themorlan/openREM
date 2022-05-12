4f67a0ae17ba5104aba94b6a54234d6e917aa54a
Jannis Widmer <jannis.widmer@ksa.ch>
2022-05-12 17:16:52 +0200

    Set wait time higher, rewrote orthanc lua such that it can execute async on windows

--- stuff/openrem_orthanc_config.lua
+++ stuff/openrem_orthanc_config.lua
@@ -38,6 +38,8 @@ local rmdir_cmd = 'rmdir /s/q'
 -- local rmdir_cmd = 'rm -r'
 -------------------------------------------------------------------------------------
 
+-- Set this to true if you use this script on windows, false otherwise
+local use_windows_execute = true
 
 -------------------------------------------------------------------------------------
 -- User-defined lists that determine how Orthanc deals with certain studies
@@ -271,7 +273,11 @@ function ToAscii(s)
  
      -- Call OpenREM import script. Runs as orthanc user in linux, so log files must be writable by Orthanc
      -- Run in detached mode, so long imports don't lead to network problems
-     os.execute(python_executable .. ' ' .. python_scripts_path .. import_script .. ' ' .. temp_file_path .. ' &')
+     if (use_windows_execute) then
+        os.execute('start /b' .. python_executable .. ' ' .. python_scripts_path .. import_script .. ' ' .. temp_file_path)
+     else
+        os.execute(python_executable .. ' ' .. python_scripts_path .. import_script .. ' ' .. temp_file_path .. ' &')
+     end
  
      -- Do not remove the dicom file. It is read async (so probably still needed and if it should be deleted after 
      -- import or not should be chosen by the user)
