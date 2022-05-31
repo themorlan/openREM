4e5446ca2635c52e6b3ae1f2a23b3750262d2f54
David Platten <dplatten@gmail.com>
2022-05-31 17:35:54 +0100

    Changed how the string is split over two lines in the source code. As it was the single quotes appeared on the form option. Now they do not. [skip ci]. Refs issue #935

--- openrem/remapp/models.py
+++ openrem/remapp/models.py
@@ -164,8 +164,8 @@ class HighDoseMetricAlertSettings(SingletonModel):
     )
     send_high_dose_metric_alert_emails_ref = models.BooleanField(
         default=False,
-        verbose_name="Send notification e-mails when alert levels for total DAP or total dose at reference point are' \
-                         'exceeded?",
+        verbose_name="Send notification e-mails when alert levels for total DAP or total dose at reference point are"
+                     " exceeded?",
     )
     send_high_dose_metric_alert_emails_skin = models.BooleanField(
         default=False,
