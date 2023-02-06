e3b2eab18406024565d8a17adbaa8a7bb4333cf9
Ed McDonagh <ed@mcdonagh.org.uk>
2023-02-06 21:41:34 +0000

    Reverting to using absolute import for models. Refs #909

--- openrem/remapp/tools/make_skin_map.py
+++ openrem/remapp/tools/make_skin_map.py
@@ -45,7 +45,7 @@ if projectpath not in sys.path:
 os.environ["DJANGO_SETTINGS_MODULE"] = "openremproject.settings"
 django.setup()
 
-from ..models import (  # pylint: disable=wrong-import-position
+from remap.models import (  # pylint: disable=wrong-import-position
     GeneralStudyModuleAttr,
     SkinDoseMapResults,
     OpenSkinSafeList
