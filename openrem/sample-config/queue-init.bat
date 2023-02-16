51dac7e15b0229eecd68603fc3a14a35b1415e1b
Kevin Schärer <kevin.schaerer@ksa.ch>
2023-02-16 16:14:26 +0100

    Updates batch file
    (Refs #963)

--- openrem/sample-config/queue-init.bat
+++ openrem/sample-config/queue-init.bat
@@ -1,11 +1,42 @@
 @echo off
+if not exist "consumer-files" mkdir consumer-files >nul
+echo ##############################
+echo # OpenREM Task Queue Starter #
+echo ##############################
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~ Removing old services ~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+set test=
+for /f "tokens=1*" %%a in ('
+    sc query state^= all ^| findstr /r /c:"SERVICE_NAME: huey-consumer-.*"
+') do (
+    echo - Stopping %%b
+    net stop %%b >nul
+    echo - Deleting %%b service
+    sc delete %%b >nul
+    echo - Deleting %%b files
+    del consumer-files\%%b* >nul
+    set test=y
+)
+if [%test%]==[] echo "Nothing to do!"
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~ Configuring new consumers ~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
 set "domain=."
 set /p "domain=Enter domain or just ENTER for default [.] : "
 set /p "username=Enter username: "
 set /p "password=Enter password: "
 set "workers=1"
 set /p "workers=How many workers would you like to spawn? [1] : "
-
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~ Instantiating new consumers ~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
 for /l %%x in (1, 1, %workers%) do (
     (
         echo ^<service^>
@@ -23,12 +54,17 @@ for /l %%x in (1, 1, %workers%) do (
         echo     ^<allowservicelogon^>true^</allowservicelogon^>
         echo   ^</serviceaccount^>
         echo ^</service^>
-    ) > "huey_worker_%%x.xml"
-    COPY WinSW.exe huey_worker_%%x.exe >nul
-    start /W "" huey_worker_%%x.exe stopwait
-    start /W "" huey_worker_%%x.exe uninstall
-    start /W "" huey_worker_%%x.exe install
-    start /W "" huey_worker_%%x.exe start
+    ) > "consumer-files\huey-consumer-%%x.xml"
+    echo Preparing huey-consumer-%%x...
+    COPY WinSW.exe consumer-files\huey-consumer-%%x.exe >nul
+    start /B /W "" consumer-files\huey-consumer-%%x.exe stopwait >nul
+    start /B /W "" consumer-files\huey-consumer-%%x.exe uninstall >nul
+    echo Installing huey-consumer-%%x as service...
+    start /B /W "" consumer-files\huey-consumer-%%x.exe install >nul
+    echo Starting huey-consumer-%%x service...
+    start /B /W "" consumer-files\huey-consumer-%%x.exe start >nul
 )
-
+echo.
+echo All done, you can now safely close this window!
+echo.
 pause
