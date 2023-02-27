2145b7d2f1df5b69e31d5a2e9750dfed9723a926
Ed McDonagh <ed@mcdonagh.org.uk>
2023-02-27 16:39:05 +0000

    Removing import made redundant in September 2020! Refs #512

--- openrem/openremproject/settings.py
+++ openrem/openremproject/settings.py
@@ -2,7 +2,6 @@
 
 import os
 import multiprocessing
-from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
