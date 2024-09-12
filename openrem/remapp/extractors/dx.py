dcc5497848b942fdadaf90889faeb7e70e042647
Ed McDonagh <ed@mcdonagh.org.uk>
2024-09-12 22:35:49 +0100

    Refactored slightly, updated test DICOM file with study time and date. Refs #1033

--- openrem/remapp/extractors/dx.py
+++ openrem/remapp/extractors/dx.py
@@ -919,6 +919,13 @@ def _fix_kodak_filters(dataset):
             dict.__setitem__(dataset, 0x187054, thick2)
 
 
+def _remove_spaces_decimal(value):
+    """Remove padding and convert to decimal"""
+    while value and value.endswith((' ', '\x00')):
+        value = value[:-1]
+    return Decimal(value)
+
+
 def _fix_exposure_values(dataset):
     """
     Replace decimal values in ExposureTime, XRayTubeCurrent and Exposure with integer values; move original values to
@@ -929,25 +936,29 @@ def _fix_exposure_values(dataset):
     try:
         dataset.ExposureTime
     except TypeError:
-        exposure_time = Decimal(dataset.get_item('ExposureTime').value.decode().replace('\x00',''))
+        exposure_time = dataset.get_item('ExposureTime').value.decode()
+        exposure_time = _remove_spaces_decimal(exposure_time)
         del dataset.ExposureTime
         dataset.ExposureTime = round(exposure_time, 0)
         dataset.ExposureTimeInuS = round(exposure_time * 1000, 0)
     try:
         dataset.XRayTubeCurrent
     except TypeError:
-        xray_tube_current = Decimal(dataset.get_item('XRayTubeCurrent').value.decode().replace('\x00',''))
+        xray_tube_current = dataset.get_item('XRayTubeCurrent').value.decode()
+        xray_tube_current = _remove_spaces_decimal(xray_tube_current)
         del dataset.XRayTubeCurrent
         dataset.XRayTubeCurrent = round(xray_tube_current, 0)
         dataset.XRayTubeCurrentInuA = round(xray_tube_current * 1000, 0)
     try:
         dataset.Exposure
     except TypeError:
-        exposure = Decimal(dataset.get_item('Exposure').value.decode().replace('\x00',''))
+        exposure = dataset.get_item('Exposure').value.decode()
+        exposure = _remove_spaces_decimal(exposure)
         del dataset.Exposure
         dataset.Exposure = round(exposure, 0)
         dataset.ExposureInuAs = round(exposure * 1000, 0)
     return dataset
+
 
 def dx(dig_file):
     """Extract radiation dose structured report related data from DX radiographic images
